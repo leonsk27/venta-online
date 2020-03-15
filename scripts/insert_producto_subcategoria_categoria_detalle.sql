9535c7f82574dad5d28de3c6e6bfbdb355f28190
carlos <carlos.ojeda.a.v@gmail.com>
2020-03-15 14:57:29 -0300

    primera version

---
+++ scripts/insert_producto_subcategoria_categoria_detalle.sql
@@ -0,0 +1,288 @@
+insert into categoria values
+(null, 'computadora');
+insert into categoria values
+(null, 'ropa');
+insert into categoria values
+(null, 'celular');
+insert into categoria values
+(null, 'juguete');
+
+
+insert into subcategoria values 
+(null, 'laptop', 1);
+insert into subcategoria values 
+(null, 'escritorio', 1);
+insert into subcategoria values 
+(null, 'tablet', 1);
+insert into subcategoria values 
+(null, 'accesorio de computadora', 1);
+insert into subcategoria values 
+(null, 'componente', 1);
+insert into subcategoria values 
+(null, 'periferico', 1);
+
+
+insert into subcategoria values 
+(null, 'polera', 2);
+insert into subcategoria values 
+(null, 'camisa', 2);
+insert into subcategoria values 
+(null, 'calzado', 2);
+insert into subcategoria values 
+(null, 'vestido', 2);
+insert into subcategoria values 
+(null, 'blusa', 2);
+insert into subcategoria values 
+(null, 'abrigo', 2);
+
+
+insert into subcategoria values 
+(null, 'celular móvil', 3);
+insert into subcategoria values 
+(null, 'accesorio de celular', 3);
+insert into subcategoria values 
+(null, 'telefono fijo', 3);
+
+
+insert into producto values
+(null, 'Lenovo Ideapad 15.6', 10, 400, 'Lenovo', 'https://images-na.ssl-images-amazon.com/images/I/61QiLcparHL._AC_SX425_.jpg',1);
+
+insert into producto values
+(null, 'HP Pavilion 15.6 Inch Touchscreen Laptop', 10, 499, 'hp', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+
+insert into producto values
+(null, 'Asus aspire 16.4', 10, 400, 'Asus', 'https://images-na.ssl-images-amazon.com/images/I/61QiLcparHL._AC_SX425_.jpg',1);
+
+insert into producto values
+(null, 'Apple pantalla tactil 15.2', 10, 520, 'Apple', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+insert into producto values
+(null, 'Vaio gamer16.2', 10, 550, 'Vaio', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+
+insert into producto values
+(null, 'Vaio pantalla tactil 15.2', 10, 500, 'Vaio', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+
+insert into producto values
+(null, 'Apple gamer pantalla tactil 17.2', 10, 520, 'Apple', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+
+insert into producto values
+(null, 'Acer pantalla tactil 14.6', 10, 499, 'Acer', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',1);
+
+
+insert into producto values
+(null, 'Apple 7', 10, 350, 'Apple', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Microsoft 6 core', 10, 390, 'Microsoft', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Motorola 7 plus', 10, 329, 'Motorola', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Samsung j2 prime', 10, 269, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Samsung j4 prime', 10, 309, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Samsung j4 plus', 10, 325, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Samsung j1 prime', 10, 250, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Samsung j4 dual core', 10, 360, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',3);
+
+insert into producto values
+(null, 'Auriculares samsung inalambrico', 10, 25, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into producto values
+(null, 'Auriculares samsung bluetooth', 10, 12, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into producto values
+(null, 'Auriculares samsung', 10, 5, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into producto values
+(null, 'Cargador samsung 2w', 10, 25, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into producto values
+(null, 'Cargador samsung 4w', 10, 35, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into producto values
+(null, 'Cargador portatil samsung 2w', 10, 50, 'Samsung', 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg',4);
+
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+insert into color values
+(null, '');
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 1);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 1);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 1);
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 2);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 2);
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 3);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 3);
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 4);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 4);
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 4);
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 5);
+
+insert into productoColor values 
+(null, 'https://images-na.ssl-images-amazon.com/images/I/817hInLq9FL._AC_SL1500_.jpg', 1, 6);
+
+
+insert into compra values 
+(null, '2020-01-01', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-01', 1, 'simon i patinho #220', 0, 2);
+insert into compra values 
+(null, '2020-01-03', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-04', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-04', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-06', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-07', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-07', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-07', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-08', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-09', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-09', 1, 'simon i patinho #220', 0, 1);
+
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-11', 1, 'simon i patinho #220', 0, 1);
+insert into compra values 
+(null, '2020-01-10', 1, 'simon i patinho #220', 0, 1);
